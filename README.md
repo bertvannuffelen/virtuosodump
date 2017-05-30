b2582c1c9030b3687c304b922e6e7cc0e728a5d2
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2017-05-30 08:28:42 +0000

    Add configuration & documentation 	modified:   README.md 	new file:   scripts/all_dumps.sql

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # virtuosodump
 A setup of Virtuoso which allows to dump the content of the RDFstore by a HTTP request
+
+Getting the whole content of a virtuoso store is typically done via sql scripts. This
+setup configures Virtuoso so that the data is dumped on a predefined location by issuing
+a HTTP request.
+
+The advantage of this approach is that the creation of a dump now can be part of any scripting
+environment without requiring phyisical access to the server.
+
+Configuration of the scripts
+----------------------------
+
+* the names of the dumped dataset files
+* the maximum size of dumped dataset files
+* the location of the dumped dataset files
+* selection of the graphs to be dumped
+
+Configuration of Virtuoso
+-------------------------
+
+* upload the configured scripts using isql (or via the conductor isql)
+* create a link in the vsp directory to the location of the dumped dataset files
+   `` ln -s /data/dumps ``
+* configure in the browsing of the dump directory via the conductor:
+    ** Open the Web Application Server/Virtual Domains & Directories.
+    ** Click on the New Directory in front of the 0.0.0.0 interface port 80.
+    ** Select the Type: Filesystem
+    ** Enter the logical path (part of the URI) fields in the Path and the relative path of the file relative to the vsp/ folder in the Physical path field ( '/dumps/' in the case of the example).
+    ** Check the box Allow Directory Browsing to enable the directory contents of the course.
+
+* configure in the browsing of the dump directory via the conductor:
+    ** Open the Web Application Server/Virtual Domains & Directories.
+    ** Click on the New Directory in front of the 0.0.0.0 interface port 80.
+    ** Select the Type: Filesystem
+    ** Enter the logical path (part of the URI) fields in the Path (e.g. /gen_dumps) and the relative path of the file relative to the vsp/ folder in the Physical path field ( '/dumps/' in the case of the example).
+    ** Enter the dumpgeneration script in the Post-processing Function field. (This script will be executed each time before the respons is sent). for example, DB.DBA.dumps_all_graph
+    ** Check the box Allow Directory Browsing to enable the directory contents of the course.
+
+
+Testing
+-------
+* upload RDF to a graph which is can be selected for dumping
+* test browsing only: http://localhost:8890/dumps
+* test generation & browsing: http://localhost:8890/gen_dumps
+
+
+
+
---
+++ scripts/all_dumps.sql
@@ -0,0 +1,157 @@
+-- using sparql to get all graphs
+CREATE PROCEDURE DB.DBA.dumps_all_graphs () 
+{
+declare N int;
+declare dataset varchar ;
+
+N:=1;
+
+for ( select graph_iri from ( sparql select distinct ?graph_iri where {graph ?graph_iri {?s a ?t}}) as sub ) do
+{
+    log_message(sprintf('Possible graph to dump %s', graph_iri)) ;
+    IF ( strcontains(graph_iri, 'dataset') ) {
+        dataset := sprintf('/data/dumps/dataset%i', N); 
+        log_message(dataset);
+        N := N +1;
+        dump_one_graph(graph_iri, dataset);
+        log_message(sprintf('graph %s dumped as dataset %s', graph_iri, dataset)) ;
+    };
+    
+};
+
+RETURN 1 ;
+
+};
+
+
+-- using builtin SPARQL_SELECT_KNOWN_GRAPHS
+-- the _T version is the view which can be queried using sql
+CREATE PROCEDURE DB.DBA.dumps_all_graphs2 () 
+{
+declare N int;
+declare dataset varchar ;
+
+N:=1;
+
+for select graph_iri from DB.DBA.SPARQL_SELECT_KNOWN_GRAPHS_T do
+{
+    log_message(sprintf('Possible graph to dump %s', graph_iri)) ;
+    IF ( strcontains(graph_iri, 'dataset') ) {
+        dataset := sprintf('/data/dumps/dataset%i', N); 
+        log_message(dataset);
+        N := N +1;
+        dump_one_graph(graph_iri, dataset);
+        log_message(sprintf('graph %s dumped as dataset %s', graph_iri, dataset)) ;
+    };
+    
+};
+
+RETURN 1 ;
+
+};
+
+
+-- count the number of triples for a graph
+CREATE PROCEDURE DB.DBA.counttrip (IN srcgraph VARCHAR) 
+{
+   RETURN ( SPARQL SELECT COUNT(*) WHERE { GRAPH `iri(?:srcgraph)` { ?s ?p ?o}} );
+}
+
+
+-- dump one graph in a series of files
+CREATE PROCEDURE DB.DBA.dump_one_graph 
+  ( IN  srcgraph           VARCHAR
+  , IN  out_file           VARCHAR
+  , IN  file_length_limit  INTEGER  := 1000000000
+  )
+  {
+    DECLARE  file_name     VARCHAR
+  ; DECLARE  env
+          ,  ses           ANY
+  ; DECLARE  ses_len
+          ,  max_ses_len
+          ,  file_len
+          ,  triplescount
+          ,  file_idx      INTEGER
+  ; SET ISOLATION = 'uncommitted'
+  ; max_ses_len  := 10000000
+  ; file_len     := 0
+  ; file_idx     := 1
+  ; triplescount :=  CAST ( DB.DBA.counttrip (srcgraph ) AS INTEGER)
+  ; file_name    := sprintf ('%s.ttl', out_file)
+  ; IF (triplescount > max_ses_len ) 
+       file_name    := sprintf ('%s_%02d.ttl', out_file, file_idx)
+    
+  ; string_to_file ( file_name || '.graph', 
+                     srcgraph, 
+                     -2
+                   );
+    string_to_file ( file_name, 
+                     sprintf ( '# Dump of graph <%s>, as of %s (size %s) \n@base <> .\n', 
+                               srcgraph, 
+                               CAST (NOW() AS VARCHAR),
+                               CAST (triplescount AS VARCHAR)
+                             ), 
+                     -2
+                   )
+  ; env := vector (dict_new (16000), 0, '', '', '', 0, 0, 0, 0, 0)
+  ; ses := string_output ()
+
+  ; FOR (SELECT * FROM ( SPARQL DEFINE input:storage "" 
+                         SELECT ?s ?p ?o { GRAPH `iri(?:srcgraph)` { ?s ?p ?o } } 
+                       ) AS sub OPTION (LOOP)) DO
+      {
+        http_ttl_triple (env, "s", "p", "o", ses);
+        ses_len := length (ses);
+        IF (ses_len > max_ses_len)
+          {
+            file_len := file_len + ses_len;
+            IF (file_len > file_length_limit)
+              {
+                http (' .\n', ses);
+                string_to_file (file_name, ses, -1);
+		gz_compress_file (file_name, file_name||'.gz');
+		file_delete (file_name);
+                file_len := 0;
+                file_idx := file_idx + 1;
+                file_name := sprintf ('%s_%02d.ttl', out_file, file_idx);
+                string_to_file ( file_name, 
+                                 sprintf ( '# Dump of graph <%s>, as of %s (part %d) (size %s) \n@base <> .\n', 
+                                           srcgraph, 
+                                           CAST (NOW() AS VARCHAR), 
+                                           file_idx,
+                               		   CAST (triplescount AS VARCHAR) ),
+                                 -2
+                               );
+                 env := VECTOR (dict_new (16000), 0, '', '', '', 0, 0, 0, 0, 0);
+		 string_to_file ( file_name || '.graph', 
+				     srcgraph, 
+				     -2
+				   );
+		 string_to_file ( file_name, 
+				     sprintf ( '# Dump of graph <%s>, as of %s (size %s) \n@base <> .\n', 
+					       srcgraph, 
+					       CAST (NOW() AS VARCHAR),
+					       CAST (triplescount AS VARCHAR)
+					     ), 
+				     -2
+				   );
+              }
+            ELSE
+              string_to_file (file_name, ses, -1);
+            ses := string_output ();
+          }
+      }
+    IF (LENGTH (ses))
+      {
+        http (' .\n', ses);
+        string_to_file (file_name, ses, -1);
+	gz_compress_file (file_name, file_name||'.gz');
+	file_delete (file_name);
+      }
+  }
+;
+
+
+
+
